f09d3d7d2710d7f2142308e33a8a9850e612ae44
Anastasia Kravchenko <142917747+mega-nastyxa@users.noreply.github.com>
2025-03-22 14:05:26 +0300

    comit

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-document.getElementById('menuButton').addEventListener('click', function() {
-    alert('Меню открыто!'); // Здесь можно добавить логику для открытия меню
+// document.getElementById('menuButton').addEventListener('click', function() {
+//     alert('Меню открыто!'); // Здесь можно добавить логику для открытия меню
+// });
+
+// document.addEventListener("DOMContentLoaded", function() {
+//    document.getElementById("menuButton").addEventListener("click", function() {
+//     document.querySelector("container__header").classList.toggle("open")
+//    })
+// })
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("menuButton").addEventListener("click", function() {
+        document.querySelector(".container__header").classList.toggle("open");
+    });
 });
